f6ba352ac8723505f95e466f80296af11ac882b9
AngelCamero <camero.angel@student.ccm.edu>
2026-09-08 16:53:52 -0400

    Working on kineticEnergy project

--- CelsiusTable.java
+++ CelsiusTable.java
@@ -1,3 +1,8 @@
+import java.util.Scanner;
 public class CelsiusTable {
-    
+    public static void main(String [] args){
+        Scanner keyboard = new Scanner(System.in);
+        
+        keyboard.close();
+    }
 }
--- KineticEnergy.java
+++ KineticEnergy.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
 public class KineticEnergy {
+    public static double kineticEnergy(double mass, double velocity){
+            double kE = 0.5 * mass * (velocity * velocity);
+            return kE;
+        }
+    public static void main(String [] args){
+        Scanner keyboard = new Scanner(System.in);
+        double mass;
+        double velocity;
+        double kineticENergy;
+        System.out.print("Enter the mass of the object: ");
+        mass = keyboard.nextDouble();
+        System.out.print("Enter the velocity of the object: ");
+        velocity = keyboard.nextDouble();
+        kineticENergy = kineticEnergy(mass, velocity);
+        System.out.printf("%.2f" kineticENergy);
 
+        keyboard.close();
+    }
 }
