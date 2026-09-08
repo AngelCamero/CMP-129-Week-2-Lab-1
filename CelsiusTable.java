6d30af3e02147a6cdd934fd9a02d3536b85a0d40
AngelCamero <camero.angel@student.ccm.edu>
2026-09-08 16:57:13 -0400

    Finished kineticEnergy, Starting celciusTable.

--- CelsiusTable.java
+++ CelsiusTable.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 public class CelsiusTable {
+    public static double 
     public static void main(String [] args){
         Scanner keyboard = new Scanner(System.in);
         
