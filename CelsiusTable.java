36d905a722be1ff2229ed14e9b961a5552017896
AngelCamero <camero.angel@student.ccm.edu>
2026-09-08 21:34:59 -0400

    Finished 2/3 problems. starting last one

--- CelsiusTable.java
+++ CelsiusTable.java
@@ -1,9 +1,12 @@
-import java.util.Scanner;
 public class CelsiusTable {
-    public static double 
-    public static void main(String [] args){
-        Scanner keyboard = new Scanner(System.in);
-        
-        keyboard.close();
+    public static double celsius(double fahrenheit){
+        double celcius = (5.0 / 9.0) * (fahrenheit - 32);
+        return celcius;
+    }
+    public static void main(String [] args){    
+        for (int fahrenheit = 0; fahrenheit <= 20; fahrenheit++){
+            double celciusValue = celsius(fahrenheit);
+            System.out.printf("%d = %.2f%n", fahrenheit, celciusValue);
+        }
     }
 }
